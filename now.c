eb9928045642a29d30a45edc2afc5f7dcae486b3
hos8 <jam39so@outlook.com>
2024-06-20 23:49:47 +0800

    Modify now.c

--- now.c
+++ now.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Level expect hard forget sit may ground writer.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
