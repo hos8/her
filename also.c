721b3ea60eec61d1950f3a2a420e58d128270e5f
hos8 <jam39so@outlook.com>
2024-07-09 01:02:44 +0800

    Modify also.c

--- also.c
+++ also.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# True rock response bed do nearly learn place.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
