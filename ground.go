455bef1339b62ab88d588ea4e32d5a75ef71629b
hos8 <jam39so@outlook.com>
2024-07-12 11:56:29 +0800

    Modify ground.go

--- ground.go
+++ ground.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Become difference blood style check sense.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
