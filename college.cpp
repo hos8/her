5578ce8c2e7b8feee0e2ae7e2d13db0b5b6f4eb1
hos8 <jam39so@outlook.com>
2024-07-20 13:55:17 +0800

    Modify college.cpp

--- college.cpp
+++ college.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Remember statement rather.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
