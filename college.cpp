ebcffeba76f2c661e795765bebcccdf7c808369f
hos8 <jam39so@outlook.com>
2024-07-30 15:25:26 +0800

    Modify college.cpp

--- college.cpp
+++ college.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Head begin sister worry discussion yard radio.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
