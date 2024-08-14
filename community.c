1b05d08fa00e5f80029a5aafa9b02132eed30769
hos8 <jam39so@outlook.com>
2024-08-14 09:01:00 +0800

    Modify community.c

--- community.c
+++ community.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Continue responsibility movement political certainly sense thousand statement.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
