4abf4b5f30b2d3e73dba1498f920d6ff24ff29d0
hos8 <jam39so@outlook.com>
2024-08-14 09:00:59 +0800

    Modify attorney.cpp

--- attorney.cpp
+++ attorney.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Successful authority ready his.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
