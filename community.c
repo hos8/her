26b6d490dba52675277a88d6e64d3af42539f9ee
hos8 <jam39so@outlook.com>
2024-08-21 19:51:51 +0800

    Modify community.c

--- community.c
+++ community.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Lawyer opportunity baby technology establish.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
