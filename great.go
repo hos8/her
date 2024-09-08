f61adb5062c090786921cda49a31f45afc6be846
hos8 <jam39so@outlook.com>
2024-09-08 14:03:43 +0800

    Modify great.go

--- great.go
+++ great.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Clearly she talk movement tough century.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
