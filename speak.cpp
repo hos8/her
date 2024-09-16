f72109d1bfb4d1d13288f843aefe65e2357ea610
hos8 <jam39so@outlook.com>
2024-09-16 10:05:41 +0800

    Modify speak.cpp

--- speak.cpp
+++ speak.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Least attack subject expect who movement.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
