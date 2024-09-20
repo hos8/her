a96c5b24ec1a91d6dfee74df2a1cdfd399f23a53
hos8 <jam39so@outlook.com>
2024-09-20 19:44:54 +0800

    Modify risk.c

--- risk.c
+++ risk.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Beautiful culture road shoulder others himself maybe.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
