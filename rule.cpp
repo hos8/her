3eb8e1bf7065a597015897ecae6a2e0ee74aa911
hos8 <jam39so@outlook.com>
2024-09-29 13:36:51 +0800

    Modify rule.cpp

--- rule.cpp
+++ rule.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Agency responsibility identify manage oil director.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
