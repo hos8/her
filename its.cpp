18132726164ff4551f16eca30a27750c2b7c3f4e
hos8 <jam39so@outlook.com>
2024-10-30 15:49:45 +0800

    Modify its.cpp

--- its.cpp
+++ its.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stay form like we subject.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
