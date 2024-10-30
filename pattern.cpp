875033d05a5e5373d9655cadb76ec85e54fe716a
hos8 <jam39so@outlook.com>
2024-10-30 15:49:45 +0800

    Modify pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Describe ask choose all fear stop.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
