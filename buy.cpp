a2ea3456f7dbfa1da3fcf0dc1cb504a8ffab61ff
hos8 <jam39so@outlook.com>
2024-12-07 20:37:40 +0800

    Modify buy.cpp

--- buy.cpp
+++ buy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mother purpose specific rule visit account cup.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
