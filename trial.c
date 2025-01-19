fc154c2c058fcc6d86433776dc7849c75a165681
hos8 <jam39so@outlook.com>
2025-01-19 14:26:15 +0800

    Modify trial.c

--- trial.c
+++ trial.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Book idea conference project certainly minute.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
