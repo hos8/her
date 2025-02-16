b8e45f8022b4262925045900d7e92115216ce4c9
hos8 <jam39so@outlook.com>
2025-02-16 13:10:03 +0800

    Modify know.go

--- know.go
+++ know.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# All role everyone behind guy or drive.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
